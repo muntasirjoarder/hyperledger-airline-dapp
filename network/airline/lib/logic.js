6cfa9adc813d4ec2db41cfdad407bbaeb7eb74fc
Muntasir Joarder <muntasir.joarder@gmail.com>
2018-03-07 11:40:36 +1000

    Models updated. Dist folder created.

--- network/airline/lib/logic.js
+++ network/airline/lib/logic.js
@@ -8,16 +8,3 @@
  * @param {org.conair.airline.ChangeAssetValue} changeAssetValue
  * @transaction
  */
-function onChangeAssetValue(changeAssetValue) {
-    var assetRegistry;
-    var id = changeAssetValue.relatedAsset.assetId;
-    return getAssetRegistry('org.conair.airline.SampleAsset')
-        .then(function(ar) {
-            assetRegistry = ar;
-            return assetRegistry.get(id);
-        })
-        .then(function(asset) {
-            asset.value = changeAssetValue.newValue;
-            return assetRegistry.update(asset);
-        });
-}
