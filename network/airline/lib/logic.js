21cf583a3297f03f7d54c695e685f2b2541eab85
Muntasir Joarder <muntasir.joarder@gmail.com>
2018-03-08 15:04:35 +1000

    working on transaction

--- network/airline/lib/logic.js
+++ network/airline/lib/logic.js
@@ -5,6 +5,41 @@
 
 /**
  * Sample transaction
- * @param {org.conair.airline.ChangeAssetValue} changeAssetValue
+ * @param {org.conair.airline.flight.createFlight} flightData
  * @transaction
  */
+function createFlight(flightData){
+    //1. get the asset registry
+    return getAssetRegistry('org.conair.airline.flight.Flight')
+        .then(function(flightRegistry){
+            //Now add flight
+            //2 get resource factory
+            var factory = getFactory();
+            var NS = 'org.conair.airline.flight';
+
+            //3. create Resource instance
+            var flightId = 'AE102-05-10-18';
+            var flight = factory.newResource(NS,flightId);
+
+            //4. set the relationship
+            flight.flightNumber = flightData.flightNumber;
+
+            // 5. Create new concept
+            var route = factory.newConcept(NS,"Route");
+
+            route.origin = flightData.origin;
+            route.destination = flightData.destination;
+            route.schedule = flightData.schedule;
+
+            flight.route = route;
+            flight.aliasFlightNumber = [];
+
+            //Emit event
+
+            var event = factory.newEvent(NS,'flightCreated');
+            event.flightId = flightId;
+            emi(event);
+
+            return flightRegistry.addAll([flight]);
+        });
+}
